8cc8d4d7c33ea4f1aa272cd4eccb53e49e9cbe34
ChoiUS <chldn323@gmail.com>
2025-05-31 17:14:54 +0900

    feat: 테스트용 함수 수정

--- app/src/main/java/com/choius323/mapleblock/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/choius323/mapleblock/ui/screen/home/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.choius323.mapleblock.ui.screen.home
 
+import android.app.Activity
+import android.content.Intent
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -11,13 +13,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.choius323.mapleblock.ui.screen.home.HomeSideEffect.ShowToast
-import kotlinx.coroutines.delay
+import com.choius323.mapleblock.ui.screen.splash.SplashActivity
 import org.koin.androidx.compose.koinViewModel
 import org.orbitmvi.orbit.compose.collectAsState
 import org.orbitmvi.orbit.compose.collectSideEffect
+
 
 @Composable
 fun HomeScreen(
@@ -29,10 +35,6 @@ fun HomeScreen(
     val uiState by viewModel.collectAsState()
     val context = LocalContext.current
 
-    LaunchedEffect(Unit) {
-        delay(1000)
-        viewModel.add()
-    }
     viewModel.collectSideEffect {
         when (it) {
             is ShowToast -> {
--- app/src/main/java/com/choius323/mapleblock/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/choius323/mapleblock/ui/screen/home/HomeViewModel.kt
@@ -1,13 +1,23 @@
 package com.choius323.mapleblock.ui.screen.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.choius323.mapleblock.ui.model.Notice
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.viewmodel.container
 
 class HomeViewModel(
 ) : ContainerHost<HomeUiState, HomeSideEffect>, ViewModel() {
     override val container = container<HomeUiState, HomeSideEffect>(initialState = HomeUiState())
+
+    init {
+        viewModelScope.launch {
+            delay(1000)
+            add()
+        }
+    }
 
     fun add() = intent {
         postSideEffect(HomeSideEffect.ShowToast("add notice ${state.notices.size}"))
